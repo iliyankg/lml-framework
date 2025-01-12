3e16e1208e41039f8e4cc7dc7ae972c99e0a51bd
Iliyan Georgiev <9661240+iliyankg@users.noreply.github.com>
2025-01-12 23:09:01 +0000

    chore(naming): update convinience type name

--- lml_python/core/vmath.py
+++ lml_python/core/vmath.py
@@ -1,6 +1,6 @@
-type _Dotable = list[int] | tuple[int, ...] | list[float] | tuple[float, ...]
+type _ListLike = list[int] | tuple[int, ...] | list[float] | tuple[float, ...]
 
 
-def dot(a: _Dotable, b: _Dotable) -> float | int:
+def dot(a: _ListLike, b: _ListLike) -> float | int:
     assert len(a) == len(b), "Dot Product requires vectors of equal length"
     return sum(x*y for x, y in zip(a, b))
